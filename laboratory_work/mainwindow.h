25fbab573952950adc584bdfcf64f003a1b0ac4e
holite <144428331+32456543@users.noreply.github.com>
2023-11-30 01:28:30 +0300

    complete

---
+++ laboratory_work/mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btn_clear_clicked();
+
+    void on_btn_1_clicked();
+
+    void on_btn_7_clicked();
+
+    void on_btn_8_clicked();
+
+    void on_btn_9_clicked();
+
+    void on_btn_6_clicked();
+
+    void on_btn_5_clicked();
+
+    void on_btn_4_clicked();
+
+    void on_btn_3_clicked();
+
+    void on_btn_2_clicked();
+
+    void on_btn_ce_clicked();
+
+    void on_btn_backspace_clicked();
+
+    void on_btn_div_clicked();
+
+    void on_btn_mul_clicked();
+
+    void on_btn_sub_clicked();
+
+    void on_btn_add_clicked();
+
+    void on_btn_calc_clicked();
+
+    void on_btn_point_clicked();
+
+    void on_btn_neg_clicked();
+
+private:
+    QString calculateExpression(const QString &expression);
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
+
